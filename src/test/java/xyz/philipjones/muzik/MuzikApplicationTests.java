978f015da1c1b032c70984aa8427269434b42ca7
Philip Jones <jones.philipr@gmail.com>
2024-09-28 11:56:45 -0400

    Initial Commit for backend

---
+++ src/test/java/xyz/philipjones/muzik/MuzikApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.philipjones.muzik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuzikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
