b916de68073261c993fa1c4494d53f4c0044230c
Philip Jones <jones.philipr@gmail.com>
2024-09-30 18:10:54 -0400

    Scrape utf-8 percent encodings

--- Tools/Harvest/main.js
+++ Tools/Harvest/main.js
@@ -1,7 +1,15 @@
-import randomString from "./randomString.js";
+import randomString from "./modules/randomString.js";
 import dotenv from "dotenv";
+import axios from "axios";
 
 dotenv.config({ path: '../../.env' });
 
 const SPOTIFY_ACCESS_TOKEN = process.env.SPOTIFY_ACCESS_TOKEN;
 
+// Make request to Spotify API
+
+
+
+// Go through each track and check if it's in the database
+
+// If it's not in the database, add it
