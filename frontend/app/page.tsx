4be2a78d529c55958f31cae371ebdff33c8f483f
Philip Roy Jones <145068574+philip-roy-jones@users.noreply.github.com>
2024-10-28 22:31:19 -0400

    Redesign login page

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="">
+      <main className="">
+        <div className="">
+          <h1>Philip Jones</h1>
+        </div>
+        <a href="/login" className="text-blue-600 underline">
+          Login
+        </a>
+      </main>
+      <footer className=""></footer>
+    </div>
+  );
+}
